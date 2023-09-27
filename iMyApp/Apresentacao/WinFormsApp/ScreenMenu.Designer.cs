fa73f2f2a66f3f581f3a4250f9ae80511c77d872
Senai <Senai@CEPEAS21104013>
2023-09-26 21:43:02 -0300

    Modificação mp ScreenMenu

--- iMyApp/Apresentacao/WinFormsApp/ScreenMenu.Designer.cs
+++ iMyApp/Apresentacao/WinFormsApp/ScreenMenu.Designer.cs
@@ -35,8 +35,6 @@
             label1 = new Label();
             contextMenuStrip1 = new ContextMenuStrip(components);
             sidebar = new FlowLayoutPanel();
-            pnDashboard = new Panel();
-            button2 = new Button();
             menuContainer = new FlowLayoutPanel();
             panel4 = new Panel();
             menu = new Button();
@@ -51,12 +49,14 @@
             button6 = new Button();
             pnLogout = new Panel();
             button7 = new Button();
+            pnDashboard = new Panel();
+            button2 = new Button();
             menuTransition = new System.Windows.Forms.Timer(components);
             sidebarTransition = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)btnHan).BeginInit();
             sidebar.SuspendLayout();
-            pnDashboard.SuspendLayout();
             menuContainer.SuspendLayout();
             panel4.SuspendLayout();
             panel2.SuspendLayout();
@@ -64,11 +64,13 @@
             pnAbout.SuspendLayout();
             pnSettings.SuspendLayout();
             pnLogout.SuspendLayout();
+            pnDashboard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = Color.White;
+            panel1.BackColor = Color.FromArgb(5, 151, 242);
             panel1.Controls.Add(btnHan);
             panel1.Controls.Add(label1);
             panel1.Location = new Point(0, -5);
@@ -91,11 +93,12 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(242, 242, 242);
             label1.Location = new Point(55, 21);
             label1.Name = "label1";
-            label1.Size = new Size(90, 17);
+            label1.Size = new Size(73, 17);
             label1.TabIndex = 2;
-            label1.Text = "WinFormsApp";
+            label1.Text = "StockGame";
             // 
             // contextMenuStrip1
             // 
@@ -104,38 +107,16 @@
             // 
             // sidebar
             // 
-            sidebar.BackColor = SystemColors.ActiveCaptionText;
-            sidebar.Controls.Add(pnDashboard);
+            sidebar.BackColor = Color.FromArgb(5, 151, 242);
             sidebar.Controls.Add(menuContainer);
             sidebar.Controls.Add(pnAbout);
             sidebar.Controls.Add(pnSettings);
             sidebar.Controls.Add(pnLogout);
+            sidebar.Controls.Add(pnDashboard);
             sidebar.Location = new Point(0, 46);
             sidebar.Name = "sidebar";
             sidebar.Size = new Size(181, 405);
             sidebar.TabIndex = 1;
-            // 
-            // pnDashboard
-            // 
-            pnDashboard.Controls.Add(button2);
-            pnDashboard.Location = new Point(3, 3);
-            pnDashboard.Name = "pnDashboard";
-            pnDashboard.Size = new Size(151, 36);
-            pnDashboard.TabIndex = 10;
-            // 
-            // button2
-            // 
-            button2.BackColor = SystemColors.ActiveCaptionText;
-            button2.ForeColor = Color.White;
-            button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.ImageAlign = ContentAlignment.MiddleLeft;
-            button2.Location = new Point(-10, -30);
-            button2.Name = "button2";
-            button2.Padding = new Padding(20, 0, 0, 0);
-            button2.Size = new Size(188, 94);
-            button2.TabIndex = 8;
-            button2.Text = "Dashboard";
-            button2.UseVisualStyleBackColor = false;
             // 
             // menuContainer
             // 
@@ -143,7 +124,8 @@
             menuContainer.Controls.Add(panel4);
             menuContainer.Controls.Add(panel2);
             menuContainer.Controls.Add(panel5);
-            menuContainer.Location = new Point(0, 42);
+            menuContainer.ForeColor = Color.FromArgb(242, 48, 100);
+            menuContainer.Location = new Point(0, 0);
             menuContainer.Margin = new Padding(0);
             menuContainer.Name = "menuContainer";
             menuContainer.Size = new Size(151, 36);
@@ -160,15 +142,15 @@
             // 
             // menu
             // 
-            menu.BackColor = SystemColors.ActiveCaptionText;
+            menu.BackColor = Color.FromArgb(5, 151, 242);
             menu.ForeColor = Color.White;
             menu.Image = (Image)resources.GetObject("menu.Image");
             menu.ImageAlign = ContentAlignment.MiddleLeft;
-            menu.Location = new Point(-9, -58);
+            menu.Location = new Point(-13, -118);
             menu.Margin = new Padding(0);
             menu.Name = "menu";
             menu.Padding = new Padding(20, 0, 0, 0);
-            menu.Size = new Size(184, 152);
+            menu.Size = new Size(196, 270);
             menu.TabIndex = 8;
             menu.Text = "Menu";
             menu.UseVisualStyleBackColor = false;
@@ -239,14 +221,15 @@
             // pnAbout
             // 
             pnAbout.Controls.Add(button5);
-            pnAbout.Location = new Point(3, 81);
+            pnAbout.ForeColor = Color.FromArgb(242, 48, 100);
+            pnAbout.Location = new Point(3, 39);
             pnAbout.Name = "pnAbout";
             pnAbout.Size = new Size(151, 36);
             pnAbout.TabIndex = 14;
             // 
             // button5
             // 
-            button5.BackColor = SystemColors.ActiveCaptionText;
+            button5.BackColor = Color.FromArgb(5, 151, 242);
             button5.ForeColor = Color.White;
             button5.Image = (Image)resources.GetObject("button5.Image");
             button5.ImageAlign = ContentAlignment.MiddleLeft;
@@ -256,20 +239,21 @@
             button5.Padding = new Padding(25, 0, 0, 0);
             button5.Size = new Size(182, 94);
             button5.TabIndex = 8;
-            button5.Text = "About";
+            button5.Text = "Salvar";
             button5.UseVisualStyleBackColor = false;
             // 
             // pnSettings
             // 
             pnSettings.Controls.Add(button6);
-            pnSettings.Location = new Point(3, 123);
+            pnSettings.ForeColor = Color.FromArgb(242, 48, 100);
+            pnSettings.Location = new Point(3, 81);
             pnSettings.Name = "pnSettings";
             pnSettings.Size = new Size(151, 36);
             pnSettings.TabIndex = 14;
             // 
             // button6
             // 
-            button6.BackColor = SystemColors.ActiveCaptionText;
+            button6.BackColor = Color.FromArgb(5, 151, 242);
             button6.ForeColor = Color.White;
             button6.Image = (Image)resources.GetObject("button6.Image");
             button6.ImageAlign = ContentAlignment.MiddleLeft;
@@ -279,20 +263,21 @@
             button6.Padding = new Padding(20, 0, 0, 0);
             button6.Size = new Size(182, 94);
             button6.TabIndex = 8;
-            button6.Text = "Settings";
+            button6.Text = "Configurações";
             button6.UseVisualStyleBackColor = false;
             // 
             // pnLogout
             // 
             pnLogout.Controls.Add(button7);
-            pnLogout.Location = new Point(3, 165);
+            pnLogout.ForeColor = Color.FromArgb(242, 48, 100);
+            pnLogout.Location = new Point(3, 123);
             pnLogout.Name = "pnLogout";
             pnLogout.Size = new Size(151, 36);
             pnLogout.TabIndex = 14;
             // 
             // button7
             // 
-            button7.BackColor = SystemColors.ActiveCaptionText;
+            button7.BackColor = Color.FromArgb(5, 151, 242);
             button7.ForeColor = Color.White;
             button7.Image = (Image)resources.GetObject("button7.Image");
             button7.ImageAlign = ContentAlignment.MiddleLeft;
@@ -302,8 +287,31 @@
             button7.Padding = new Padding(20, 0, 0, 0);
             button7.Size = new Size(182, 94);
             button7.TabIndex = 8;
-            button7.Text = "Logout";
+            button7.Text = "Sair";
             button7.UseVisualStyleBackColor = false;
+            // 
+            // pnDashboard
+            // 
+            pnDashboard.Controls.Add(button2);
+            pnDashboard.ForeColor = Color.FromArgb(242, 48, 100);
+            pnDashboard.Location = new Point(3, 165);
+            pnDashboard.Name = "pnDashboard";
+            pnDashboard.Size = new Size(151, 36);
+            pnDashboard.TabIndex = 10;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(5, 151, 242);
+            button2.ForeColor = Color.White;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(-10, -30);
+            button2.Name = "button2";
+            button2.Padding = new Padding(20, 0, 0, 0);
+            button2.Size = new Size(188, 94);
+            button2.TabIndex = 8;
+            button2.Text = "Deletar";
+            button2.UseVisualStyleBackColor = false;
             // 
             // menuTransition
             // 
@@ -314,12 +322,25 @@
             sidebarTransition.Interval = 10;
             sidebarTransition.Tick += sidebarTransition_Tick;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(233, 60);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(487, 378);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
             // ScreenMenu
             // 
             AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.FromArgb(242, 242, 242);
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
             Controls.Add(sidebar);
             Controls.Add(panel1);
+            DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.None;
             Name = "ScreenMenu";
             StartPosition = FormStartPosition.CenterScreen;
@@ -328,7 +349,6 @@
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)btnHan).EndInit();
             sidebar.ResumeLayout(false);
-            pnDashboard.ResumeLayout(false);
             menuContainer.ResumeLayout(false);
             panel4.ResumeLayout(false);
             panel2.ResumeLayout(false);
@@ -336,6 +356,8 @@
             pnAbout.ResumeLayout(false);
             pnSettings.ResumeLayout(false);
             pnLogout.ResumeLayout(false);
+            pnDashboard.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -366,5 +388,6 @@
         private System.Windows.Forms.Timer sidebarTransition;
         private Button button8;
         private PictureBox btnHan;
+        private PictureBox pictureBox1;
     }
 }
